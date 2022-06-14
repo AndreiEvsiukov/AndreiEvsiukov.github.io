4de709e3413f9c1b77b2e5d1be4a2ebcc83d9f12
Andrei <andreievsiukov@gmail.com>
2022-06-14 15:11:16 +0200

    Progress

--- assets/js/my_script.js
+++ assets/js/my_script.js
@@ -1,3 +1,5 @@
+// 1. FOR SLIDESHOW
+
 // Get all the buttons into a node list
 let buttons = document.querySelectorAll("._carousel_control");
 
@@ -14,3 +16,57 @@ document.addEventListener("click", function(evt){
     evt.target.classList.add("active");
   }
 });
+
+
+
+// 2. FOR MODALS
+
+
+
+// modal 1
+
+// Get the modal
+var modal_scheme = document.getElementById("myModal1");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img_scheme = document.getElementById("_img_diagram");
+var modalImg_scheme = document.getElementById("img01");
+var captionText_scheme = document.getElementById("caption1");
+img_scheme.onclick = function(){
+  modal_scheme.style.display = "block";
+  modalImg_scheme.src = this.src;
+  captionText_scheme.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span_scheme = document.getElementsByClassName("close1")[0];
+
+// When the user clicks on <span> (x), close the modal
+span_scheme.onclick = function() { 
+  modal_scheme.style.display = "none";
+}
+
+
+
+// modal 2
+
+// Get the modal
+var modal_ielts = document.getElementById("myModal2");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img_ielts = document.getElementById("_img_ielts");
+var modalImg_ielts = document.getElementById("img02");
+var captionText_ielts = document.getElementById("caption2");
+img_ielts.onclick = function(){
+  modal_ielts.style.display = "block";
+  modalImg_ielts.src = this.src;
+  captionText_ielts.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span_ielts = document.getElementsByClassName("close2")[0];
+
+// When the user clicks on <span> (x), close the modal
+span_ielts.onclick = function() { 
+  modal_ielts.style.display = "none";
+}
